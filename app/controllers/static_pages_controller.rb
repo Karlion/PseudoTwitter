eab305eb71960d1beda962c1adbd2f66c4bfc025
karlion <maksim.mykhaylenko@gmail.com>
2020-12-14 22:52:37 +0200

    Add project

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+class StaticPagesController < ApplicationController
+  def home
+    if signed_in?
+      @feed_items = current_user.feed.includes(:user).paginate(page: params[:page])
+    end
+  end
+
+  def help
+  end
+
+  def contact
+  end
+end
